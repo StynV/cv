7dcd9069847444c654a852051d873aead3a46a4a
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2023-10-13 13:59:02 +0200

    update intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,16 +1,19 @@
 import { useTranslation } from 'next-i18next'
+import YGWYS from '../YGWYS/YGWYS'
 import styles from './intro.module.scss'
 
 const Intro = () => {
-    const { t } = useTranslation('common')
+  const { t } = useTranslation('common')
 
-    return (
-        <article className={styles.intro}>
-          <p>{t('intro1')}</p>
-          <p>{t('intro2')}</p>
-          <p>{t('intro3')}</p>
-        </article>
-    )
+  const html: string = t('intro2')
+
+  return (
+      <article className={styles.intro}>
+        <p className={styles.text}>{t('intro1')}</p>
+        <YGWYS html={html} className={styles.text} />
+        <p className={styles.text}>{t('intro3')}</p>
+      </article>
+  )
 }
 
 export default Intro
