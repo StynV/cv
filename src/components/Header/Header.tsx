147290ef510ed6fd29c51eb1197b20688b65c01d
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2023-10-15 15:00:26 +0200

    make more responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,14 @@ const Header = () => {
 
     return (
         <header className={styles.header}>
+            <Image
+                src="https://drive.google.com/uc?export=view&id=1FJX2VHTNg6VWSHIs8BmkPcr4ZtSeIOHy"
+                alt="My image"
+                width={200}
+                height={300}
+                className={styles.img}
+            />
+            
             <div className={styles.text}>
                 <h1 className={styles.title}>{t('name')}</h1>
                 <h2 className={styles.function}>{t('function')}</h2>
@@ -25,14 +33,6 @@ const Header = () => {
                     <h6 className={styles.city}>{t('city')}</h6>
                 </div>
             </div>
-
-            <Image
-                src="https://drive.google.com/uc?export=view&id=1FJX2VHTNg6VWSHIs8BmkPcr4ZtSeIOHy"
-                alt="My image"
-                width={200}
-                height={300}
-                className={styles.img}
-            />
         </header>
     )
 }
