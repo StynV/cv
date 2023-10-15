5d3d7d97c4448b08e124128896ce06147248f4b6
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2023-10-15 14:34:26 +0200

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import styles from './footer.module.scss'
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <a
+            href="https://github.com/StynV/cv"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <Image
+                src={'/images/Github.png'}
+                alt={'Source code'}
+                width={80}
+                height={80}
+            />
+        </a>
+    </footer>
+)
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import styles from '@/styles/Home.module.scss'
-import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Header from '@/components/Header/Header'
 import Intro from '@/components/Intro/Intro'
 import Meta from '@/components/Meta/Meta'
 import WorkExperience from '@/components/WorkExperience/WorkExperience'
 import Education from '@/components/Education/Education'
+import Footer from '@/components/Footer/Footer'
+import styles from '@/styles/Home.module.scss'
 
 const Home = () => (
   <>
@@ -16,6 +16,7 @@ const Home = () => (
       <Intro />
       <WorkExperience />
       <Education />
+      <Footer />
     </main>
   </>
 )
