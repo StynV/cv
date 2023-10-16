9cd8ac1a15b6f0bc18e12cd5ddef4b2aa556aca9
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2023-10-16 09:48:20 +0200

    voeg pdf versie toe

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -31,6 +31,12 @@ const Header = () => {
                 <div>
                     <h5 className={styles.city}>{t('city')}</h5>
                 </div>
+
+                <div>
+                    <a href='https://drive.google.com/file/d/1GNA3IrEMLBXrP5DkZCpFXsZoQ2MRWC_m/view?usp=sharing' target="_blank">
+                        <h6 className={styles.city}>{t('pdf')}</h6>
+                    </a>
+                </div>
             </div>
         </header>
     )
