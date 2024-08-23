71940cfc7652f6dce259e899e407df36a2701f5b
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-08-23 11:34:17 +0200

    add linkedin logo to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,21 @@ import styles from './footer.module.scss'
 const Footer = () => (
     <footer className={styles.footer}>
         <a
+            href="https://www.linkedin.com/in/styn-vercauteren-96918018b/"
+            target="_blank"
+            rel="noreferrer"
+            itemProp='author'
+        >
+            <Image
+                src={'/images/linkedin.webp'}
+                alt={'Source code'}
+                width={70}
+                height={70}
+                className={styles.linkedinLogo}
+            />
+        </a>
+
+        <a
             href="https://github.com/StynV/cv"
             target="_blank"
             rel="noreferrer"
